1f1eaea4d4e26b5319311a77932640328463bc97
Seokwon Jang <imsoboy2@gmail.com>
2021-08-25 08:49:41 +0900

    Create make_json.py

---
+++ outband/make_json.py
@@ -0,0 +1,30 @@
+for i in range(1, 5001):
+    filename = "flow_br0_in_p2_out_p1_%d.json" % i
+    with open(filename, "w") as f:
+        data = ''' {
+  "flows": [
+    {
+      "priority": "1",
+      "timeout": 1,
+      "isPermanent": false,
+      "deviceId": "of:0000daeaadf8c24e",
+      "treatment": {
+        "instructions": [
+          {
+            "type": "OUTPUT",
+            "port": "1"
+          }
+        ]
+      },
+      "selector": {
+        "criteria": [
+          {
+            "type": "IN_PORT",
+            "port": "2"
+          }
+        ]
+      }
+    }
+  ]
+}'''
+        f.write(data)
